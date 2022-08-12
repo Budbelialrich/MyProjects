9fca78fab68dc85f584834df1a092747c3be1575
Igor Budarin <igor.budarin.17@mail.ru>
2022-08-12 14:53:43 +0300

    Выполнено! Beta 1.0

---
+++ HomeWorks/Lesson8/Easy8.java
@@ -0,0 +1,57 @@
+package HomeWorks.Lesson8;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+//„то умеет эта программа:
+//установка значенй в €чейку поочереди
+//вывод значени€ выбранной €чейки
+//очистка нужной €чейки
+//получение информации о всем массиве
+
+public class Easy8 {
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        List<Integer> arrayList = new ArrayList<>(100);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¬ведите команду: set, get, del, data или stop");
+        while (true) {
+
+            String input = scanner.nextLine();
+
+            if (input.equals("stop"))
+                break;
+
+            switch (input) {
+                case "set":
+                    System.out.println("¬ведите значение: ");
+                    int a = scanner.nextInt();
+                    arrayList.add(i, a);
+                    System.out.println("ячейка записана под номером: " + i + "!");
+                    i++;
+                    break;
+
+                case "get":
+                    System.out.println("¬ведите номер €чейки: ");
+                    int c = scanner.nextInt();
+                    System.out.println("«начение из €чейки: " + arrayList.get(c));
+                    break;
+
+                case "del":
+                    System.out.println("¬ведите номер €чейки: ");
+                    int z = scanner.nextInt();
+                    arrayList.remove(z);
+                    System.out.println("ячейка очищена!");
+                    break;
+
+                case "data":
+                    System.out.println(arrayList);
+                    break;
+            }
+        }
+    }
+}
